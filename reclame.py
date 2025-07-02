90dafdaef8994738f55906cb107c4b597dd00edd
Sergio Petri <sergiopaolopetri@gmail.com>
2025-07-02 23:15:26 +0200

    bestand algemene_functies.py toegevoegd

---
+++ reclame.py
@@ -0,0 +1,44 @@
+from algemene_functies import mijn_functie_2
+
+def aanbieding_1(smaak, prijs, korting):
+    prijs_na_korting = prijs * (1 - korting)
+    uitvoer = f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {prijs_na_korting} euro."
+    return uitvoer
+
+print(aanbieding_1("aardbei", 4, 0.1))
+
+def inkomsten_totaal(inkomsten, btw):
+    totaal = 0
+    for bedrag in inkomsten:
+        totaal += bedrag
+    btw_bedrag = totaal * btw
+    uitvoer = f"Het totaal van alle inkomsten van deze week is {totaal} euro, waarover {btw_bedrag} euro btw betaald dient te worden."
+    return uitvoer
+
+print(inkomsten_totaal([220, 430, 125, 160, 205, 90, 345], 0.09))
+
+def laag_en_hoog(mijn_lijst):
+    laagste = min(mijn_lijst)
+    hoogste = max(mijn_lijst)
+    uitvoer = (laagste, hoogste)
+    return uitvoer
+
+print(laag_en_hoog([220, 430, 125, 160, 205, 90, 345]))
+
+def gemiddelde(mijn_lijst):
+    totaal = sum(mijn_lijst)
+    aantal = len(mijn_lijst)
+    if aantal == 0:
+        return 0
+    gemiddelde = totaal / aantal
+    uitvoer = f"De gemiddelde inkomsten deze week zijn {gemiddelde} euro."
+    return uitvoer
+
+print(gemiddelde([220, 430, 125, 160, 205, 90, 345]))
+
+def meervoudig(invoer_lijst):
+    global laag_en_hoog
+    laagste, hoogste = laag_en_hoog(invoer_lijst)
+    return laagste, hoogste
+
+print(meervoudig([10, 5, 3, 2, 1, 2, 9]))
