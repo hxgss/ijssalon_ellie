5a7fa823f6c848245cff9d7d1baac829a0894c63
Sergio Petri <sergiopaolopetri@gmail.com>
2025-07-07 17:48:39 +0200

    Functie toegevoegd

--- helper.py
+++ helper.py
@@ -6,3 +6,9 @@ def decoreer(tekst=""):
     print(lengte * "*")
     print()
     
+def fooi_pp(bedrag, personen):
+    try:
+        bedrag_pp = bedrag / personen
+    except:
+        bedrag_pp = "??"
+    return f"Het bedrag van per persoon is {bedrag_pp} euro."
